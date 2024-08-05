15e85058bcc09a69eca09534c09ffdb8664e28a4
Amir M. Parvizi <official.parvizi@gmail.com>
2024-08-05 20:05:35 +0330

    Final

--- database.py
+++ database.py
@@ -1,47 +1,7 @@
-#database for local
-""" import sqlite3
-from flask import g
-
-DATABASE = 'appointment_system.db'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-def close_db(e=None):
-    db = g.pop('_database', None)
-    if db is not None:
-        db.close()
-
-def init_db(app):
-    with app.app_context():
-        db = get_db()
-        cursor = db.cursor()
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS appointments (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                specialty TEXT,
-                doctor_id INTEGER,
-                appointment_date TEXT,
-                appointment_time TEXT,
-                patient_name TEXT,
-                phone_number TEXT,
-                email TEXT,
-                national_id TEXT,
-                gender TEXT,
-                insurance_type TEXT,
-                medical_history TEXT
-            )
-        ''')
-        db.commit() """
-
-#database for vercel
 import sqlite3
 from flask import g
 
-DATABASE = ':memory:'
+DATABASE = 'appointment_system.db'
 
 def get_db():
     db = getattr(g, '_database', None)
